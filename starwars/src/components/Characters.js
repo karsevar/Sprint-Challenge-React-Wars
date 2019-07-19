c2f9249313306307140e4d60123bb523715963a9
masonkarsevar <masonkarsevar@gmail.com>
2019-07-19 08:18:24 -0700

    Characters.js axios call is working. App.js and Characters.js are now connected

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,2 +1,32 @@
-import React, {useState, UseEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+
+function Characters() {
+    const [characters, setCharacters] = useState();
+
+    useEffect(() => {
+
+        // Will use a for loop to get more than ten characters per initial mount.
+        const counter = 10;
+
+        axios  
+            .get(`https://swapi.co/api/people/`)
+            .then(response => {
+
+                // Checking if the call is receiving back data.
+                // console.log(response.data) // Test was a success. data is being received by component.
+            })
+            .catch(error => {
+                console.log('error', error);
+            })
+
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Characters;
